d8df490e46885f4ac3cfe50e6fbc00b60176feb8
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2017-11-03 11:28:27 +0000

    State does not change when game won or lost

--- test/game_test.exs
+++ test/game_test.exs
@@ -11,9 +11,16 @@ defmodule GameTest do
     assert length(game.letters) > 0
   end
 
-  test "new_game letters are lowercase a to z" do
+  test "new_game letters are all lowercase a to z" do
     game = Game.new_game()
     assert Enum.all?(game.letters, &(Regex.match?(~r/[a-z]/, &1)))
+  end
+
+  test "state isn't changes to :won or :lost game" do
+    for state <- [:won, :lost] do
+      game = Game.new_game() |> Map.put(:game_state, state)
+      assert {^game, _} = Game.make_move(game, "x")
+    end
   end
 
 end
